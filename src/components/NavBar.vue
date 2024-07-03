f70a96d1ff07c1e6453cc010d645b03c98b4f229
John Knotts <jhnnyk@gmail.com>
2024-07-03 16:17:10 -0600

    navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<script setup></script>
+
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/weirdchicken.png" alt="avatar" />
+      <h1><RouterLink :to="{ name: 'Home' }">Playlist Project</RouterLink></h1>
+      <div class="links">
+        <button>Logout</button>
+        <RouterLink class="btn" :to="{ name: 'Signup' }">Sign up</RouterLink>
+        <RouterLink class="btn" :to="{ name: 'Login' }">Log in</RouterLink>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+nav h1 {
+  margin-left: 20px;
+}
+
+nav .links {
+  margin-left: auto;
+}
+
+nav .links a,
+button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+
+nav img {
+  max-height: 60px;
+}
+</style>
