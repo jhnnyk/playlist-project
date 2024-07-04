d2aff4d32ceb1609ff8c7ea47f22eb36e6728e93
John Knotts <jhnnyk@gmail.com>
2024-07-04 15:43:32 -0600

    display playlist (realtime listener

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import getCollection from '@/composables/getCollection'
+
+const { error, documents } = getCollection('playlists')
+</script>
 
 <template>
   <div class="home">
-    <p>Homepage</p>
+    <div v-if="error" class="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">
+        {{ doc.title }}
+      </div>
+    </div>
   </div>
 </template>
